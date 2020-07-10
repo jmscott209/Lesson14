c627faf923ac792f9a04cdff5c17e94e80311d70
jmscott209 <62868845+jmscott209@users.noreply.github.com>
2020-07-10 16:30:22 -0400

    Add files via upload

---
+++ precache-manifest.4d517bebd2c3191029c47ab74161b523.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85838cc39b5cdb1ae4980ed7fb94262c",
+    "url": "/jmscott/WEB215/Lesson14/index.html"
+  },
+  {
+    "revision": "b40cd66be30a0dcf1b94",
+    "url": "/jmscott/WEB215/Lesson14/static/css/main.ca54370c.chunk.css"
+  },
+  {
+    "revision": "12230a1ed60b18584d18",
+    "url": "/jmscott/WEB215/Lesson14/static/js/2.2fb10bbd.chunk.js"
+  },
+  {
+    "revision": "ddb934a41eb6bf13706c65fe3758fb10",
+    "url": "/jmscott/WEB215/Lesson14/static/js/2.2fb10bbd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b40cd66be30a0dcf1b94",
+    "url": "/jmscott/WEB215/Lesson14/static/js/main.89359081.chunk.js"
+  },
+  {
+    "revision": "b9715e3a265f489631f4",
+    "url": "/jmscott/WEB215/Lesson14/static/js/runtime-main.cf3d248a.js"
+  },
+  {
+    "revision": "17d78e430b9c538931b168a17646e973",
+    "url": "/jmscott/WEB215/Lesson14/static/media/folio1.17d78e43.jpg"
+  },
+  {
+    "revision": "85c10cff308a263647a0160cacef3946",
+    "url": "/jmscott/WEB215/Lesson14/static/media/folio2.85c10cff.jpg"
+  },
+  {
+    "revision": "6c8202cbcb2d45434f54bd0eb487b91d",
+    "url": "/jmscott/WEB215/Lesson14/static/media/folio3.6c8202cb.jpg"
+  },
+  {
+    "revision": "cd53e576644795e167dfd60923318a06",
+    "url": "/jmscott/WEB215/Lesson14/static/media/folio4.cd53e576.jpg"
+  },
+  {
+    "revision": "1ad25639ad93b23ddadd24fb404ba308",
+    "url": "/jmscott/WEB215/Lesson14/static/media/folio5.1ad25639.jpg"
+  },
+  {
+    "revision": "b70a2248cb20d9a17625a47642dc52c4",
+    "url": "/jmscott/WEB215/Lesson14/static/media/folio6.b70a2248.jpg"
+  },
+  {
+    "revision": "93b66e9ff48210b3509d234611983a90",
+    "url": "/jmscott/WEB215/Lesson14/static/media/folio7.93b66e9f.jpg"
+  },
+  {
+    "revision": "e38cc265d4f8181189078e2c96f7c223",
+    "url": "/jmscott/WEB215/Lesson14/static/media/folio8.e38cc265.jpg"
+  },
+  {
+    "revision": "39d9dc60b051cca6747c351d2d840f7d",
+    "url": "/jmscott/WEB215/Lesson14/static/media/folio9.39d9dc60.jpg"
+  },
+  {
+    "revision": "98e3af1ea688be14e047bd2f0944e0e2",
+    "url": "/jmscott/WEB215/Lesson14/static/media/me.98e3af1e.jpg"
+  }
+]);
